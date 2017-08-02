051d0639d3b09c0d0ae1ed64e49b168b093ed17c
dimaskin <dimaskin-nv@yandex.ru>
2017-08-02 18:24:51 +0300

    add 4 suits in utf8 to pack_of_cards.rb

--- pack_of_cards.rb
+++ pack_of_cards.rb
@@ -1,4 +1,17 @@
 class PackOfCards
+  CLUBS  = "\u2663".encode('utf-8') #трефы
+  DIAMS  = "\u2666".encode('utf-8') #бубны
+  HEARTS = "\u2661".encode('utf-8') #червы
+  SPADES = "\u2664".encode('utf-8') #пики
+
   def initilize
+
+  end
+
+  def print_clubs
+  	p CLUBS
+  	p DIAMS
+    p HEARTS
+    p SPADES
   end
 end
