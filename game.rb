3e3c734153ca57708946c6a27251f69cd92396e7
dimaskin <dimaskin-nv@yandex.ru>
2017-08-11 18:12:00 +0300

    add won condition if 21

--- game.rb
+++ game.rb
@@ -1,14 +1,51 @@
 class Game
   
   def calc_sum(cards)
-    p cards
+    val_arr = cards.map{|e| e[0] }
+    total = 0
+    val_arr.each do |value|
+      if value == "A"
+        total += 11
+      elsif value.to_i == 0 #J, Q, K
+          total += 10
+      else
+          total += value.to_i
+      end
+    end
+    total
+  end
+
+  def sky_net_won
+    p "sky net won!"
+    command_list
+    #exit
+  end
+
+  def you_won
+    p "you won!"
+    command_list
+    #exit
   end
 
 
   def next_step
     p "Welcome to Blackjack!"
     cards = Deck.new.create_deck
-    calc_sum(cards)
+    sky_net = Player.new('sky_net')
+    p 'your name?'
+    human   = Player.new(gets.chomp.to_s)
+
+    sky_net.cards << cards.pop << cards.pop # 2 times
+    human.cards   << cards.pop << cards.pop
+    sky_net_sum = calc_sum(sky_net.cards)
+    human_sum   = calc_sum(human.cards)
+
+    p "sky_net_sum #{sky_net_sum}"
+    p "human_sum #{human_sum}"
+
+
+    sky_net_won if sky_net_sum == 21
+    you_won     if human_sum   == 21
   end
 
   def command_list
@@ -22,7 +59,8 @@ class Game
       answer = gets.chomp.to_i
       if answer == 1 
         next_step
-      elsif answer == 2 
+      elsif answer == 2
+        p 'bay'
         break
       else
         p '1 or 2'
--- player.rb
+++ player.rb
@@ -1,12 +1,13 @@
 class Player
-  attr_accessor :credit, :name
+  attr_accessor :credit, :name, :cards
 
   def initialize(name)
     @credit = 100
+    @cards  = []
   end
 
   def add_money
   	@credit += 10
   end
-  
+
 end
